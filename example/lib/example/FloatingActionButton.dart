fc7917357fbe6a5fdbeb6c80fdce3c87c65885ae
arpit-zibma <72298268+arpit-zibma@users.noreply.github.com>
2021-11-12 16:39:54 +0530

    Initial commit

---
+++ example/lib/example/FloatingActionButton.dart
@@ -0,0 +1,20 @@
+import 'package:firexcode/firexcode.dart';
+
+class ExampleFloatingActionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 'FloatingActionButton'.xTextColorWhite().toCenter().xScaffold(
+          backgroundColor: Colors.white,
+          appBar: 'Flaoting Action Button'.xTextColorBlack().xAppBar(
+            preferredSize: Size(15, 15),
+                brightness: Brightness.light,
+                centerTitle: true,
+                backgroundColor: Colors.white,
+              ),
+          floatingActionButton: <Widget>[
+            Icons.add.xIcons().xFloatinActionExtend(),
+            'extend'.text().xFloatinActionExtend()
+          ].xRowSSB().xlp(value: 50.0),
+        );
+  }
+}
